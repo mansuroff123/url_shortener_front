83a0d1a0da53973a513214a24f5ad1a7f93a9500
mansuroff123 <mansuroff145@gmail.com>
2026-01-07 17:26:13 +0500

    Added solidjs router and tested admin dashboard

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,18 @@
-/* @refresh reload */
-import { render } from 'solid-js/web'
-import './index.css'
-import App from './App.tsx'
+import { render } from "solid-js/web";
+import { Router, Route } from "@solidjs/router";
+import App from "./App";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import "./index.css";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-render(() => <App />, root!)
+render(
+  () => (
+    <Router root={App}>
+      <Route path="/" component={Home} />
+      <Route path="/admin" component={Admin} />
+    </Router>
+  ),
+  root!
+);
---
+++ src/pages/Admin.tsx
@@ -0,0 +1,9 @@
+function Admin() {
+    return (
+        <div class="min-h-screen bg-slate-900 text-white flex items-center justify-center">
+            <h1 class="text-4xl font-bold">Welcome to Admin Dashboard🔐</h1>
+        </div>
+    )
+}
+
+export default Admin
